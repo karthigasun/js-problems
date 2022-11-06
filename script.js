38730b796a97f92f8586a19207cdcf30c1997ac1
karthigasun <prkarthik3@gmail.com>
2022-11-06 21:19:53 +0530

    js

--- script.js
+++ script.js
@@ -27,9 +27,76 @@ arr.filter(function name(a) {
         console.log(a);
     }
 });
-let a=[1,2],b=[6,7],c;
+// let a=[1,2],b=[6,7],c;
 
-c=a;
-a=b;
+// c=a;
+// a=b;
+// console.log(a);
+// console.log(c);
+
+// numi=parseInt(prompt("Enter the number"));
+
+// if(numi==2||numi==5||numi==7){
+//     console.log("prime number" + numi);
+// }
+// else if(numi%2!==0||numi%3!==0||numi%5!==0||numi%7!==0){
+//     console.log("prime number" + numi);
+// }
+// else{
+//     console.log("not a prime number" + numi);
+// }
+
+// let word="level",wor1="";
+
+// for(i=word.length-1;i>=0;i--){
+//     wor1+=word[i];
+// }
+//     //console.log(wor1);
+//     if(word==wor1){
+//         console.log("palindrome",word);
+//     }
+//     else{
+//         console.log("not palindrome",word);
+//     }
+let arr2=[3,0,2,6,0,0];
+arr2.splice(1,1);
+console.log(arr2);
+
+//Destructuring
+let a,b,rest;
+[a,b,...rest]=[1,2,4,5,6];
 console.log(a);
-console.log(c);
+console.log(b);
+console.log(rest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
